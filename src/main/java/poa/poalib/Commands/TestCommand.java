aa395fb06e083464ea574e7c20c893a3635ac866
BuildTools <unconfigured@null.spigotmc.org>
2025-01-02 05:13:29 +0000

    updated

--- src/main/java/poa/poalib/Commands/TestCommand.java
+++ src/main/java/poa/poalib/Commands/TestCommand.java
@@ -1,16 +1,24 @@
 package poa.poalib.Commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import poa.poalib.Economy.Economy;
+import poa.poalib.economyshopgui.EconomyShop;
 
 public class TestCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        sender.sendMessage(Economy.getBalance(Bukkit.getOfflinePlayer(args[0])) + "");
-            return false;
+       if(!(sender instanceof Player player))
+           return false;
+
+       //player.sendRichMessage(EconomyShop.getSellPrice(player.getInventory().getItemInMainHand(), player) + "");
+
+
+        return false;
     }
 }
--- src/main/java/poa/poalib/economyshopgui/EconomyShop.java
+++ src/main/java/poa/poalib/economyshopgui/EconomyShop.java
@@ -5,31 +5,32 @@ import me.gypopo.economyshopgui.api.objects.BuyPrice;
 import me.gypopo.economyshopgui.api.objects.SellPrice;
 import me.gypopo.economyshopgui.util.EcoType;
 import me.gypopo.economyshopgui.util.EconomyType;
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Optional;
 
 public class EconomyShop {
 
-    private static final EcoType ecoType = EconomyType.getFromString("VAULT");
+    // THIS WORKS JUST NOT INSIDE A LIB APPARENTLY
 
-    public static double getBuyPrice(ItemStack item, OfflinePlayer player) {
-        if(player == null)
-            throw new RuntimeException("player must not be null");
-
-        final Optional<BuyPrice> buyPrice = EconomyShopGUIHook.getBuyPrice(player, item);
-        return buyPrice.map(price -> price.getPrice(ecoType)).orElse(-1.0);
-    }
-
-    public static double getSellPrice(ItemStack item, OfflinePlayer player) {
-        if(player == null)
-            throw new RuntimeException("player must not be null");
-
-        final Optional<SellPrice> sellPrice = EconomyShopGUIHook.getSellPrice(player, item);
-        return sellPrice.map(price -> price.getPrice(ecoType)).orElse(-1.0);
-    }
+//    public static double getBuyPrice(ItemStack item, OfflinePlayer player) {
+//        if(player == null)
+//            throw new RuntimeException("player must not be null");
+//
+//        final Optional<BuyPrice> buyPrice = EconomyShopGUIHook.getBuyPrice(player, item);
+//        return buyPrice.map(price -> price.getPrice(EconomyType.getFromString("VAULT"))).orElse(-1.0);
+//    }
+//
+//    public static double getSellPrice(ItemStack item, OfflinePlayer player) {
+//        final Optional<SellPrice> sellPrice = EconomyShopGUIHook.getSellPrice(player, item);
+//        if(sellPrice.isEmpty())
+//            return -1;
+//        return sellPrice.get().getPrice(null);
+//    }
 
 }
