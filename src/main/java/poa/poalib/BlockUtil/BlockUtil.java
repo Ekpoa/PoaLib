7a6fc296e52d56ba02b2790d98b99198e340bcc4
BuildTools <unconfigured@null.spigotmc.org>
2025-02-21 21:18:32 +0000

    updated 1214

--- src/main/java/poa/poalib/BlockUtil/BlockUtil.java
+++ src/main/java/poa/poalib/BlockUtil/BlockUtil.java
@@ -1,9 +1,13 @@
 package poa.poalib.BlockUtil;
 
+import de.tr7zw.changeme.nbtapi.NBTChunk;
+import de.tr7zw.changeme.nbtapi.NBTCompound;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Bisected;
 import org.bukkit.util.BoundingBox;
 
 import java.util.ArrayList;
@@ -42,6 +46,27 @@ public class BlockUtil {
                     tr.add(world.getBlockAt(new Location(world,x,y,z)));
 
         return tr;
+    }
+
+    public static void removeBlockNBT(Block block) {
+        NBTCompound chunkContainer = new NBTChunk(block.getChunk()).getPersistentDataContainer();
+        if (chunkContainer.hasTag("blocks")) {
+            NBTCompound blocksContainer = chunkContainer.getOrCreateCompound("blocks");
+            removeNBT(blocksContainer, block);
+
+            if (block.getBlockData() instanceof Bisected bisected) {
+                BlockFace face = bisected.getHalf() == Bisected.Half.BOTTOM ? BlockFace.UP : BlockFace.DOWN;
+                removeNBT(blocksContainer, block.getRelative(face));
+            }
+            if (blocksContainer.getKeys().isEmpty()) chunkContainer.removeKey("blocks");
+        }
+    }
+
+    private static void removeNBT(NBTCompound blocksContainer, Block block) {
+        String blockKey = String.format("%s_%s_%s", block.getX(), block.getY(), block.getZ());
+        if (blocksContainer.hasTag(blockKey)) {
+            blocksContainer.removeKey(blockKey);
+        }
     }
 
 
