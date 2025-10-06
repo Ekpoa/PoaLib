eb060a334b9fab490d9be7aff7ed2be991d7070e
BuildTools <unconfigured@null.spigotmc.org>
2025-10-06 22:43:01 +0100

    updated

---
+++ src/main/java/poa/poalib/entity/PlayerUtil.java
@@ -0,0 +1,40 @@
+package poa.poalib.entity;
+
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.util.BoundingBox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerUtil {
+    public static List<Block> getBlocksInHitbox(Player player) {
+        BoundingBox box = player.getBoundingBox().expand(0.05); // small buffer for precision
+        World world = player.getWorld();
+        List<Block> blocks = new ArrayList<>();
+
+        int minX = (int) Math.floor(box.getMinX());
+        int maxX = (int) Math.floor(box.getMaxX());
+        int minY = (int) Math.floor(box.getMinY());
+        int maxY = (int) Math.floor(box.getMaxY());
+        int minZ = (int) Math.floor(box.getMinZ());
+        int maxZ = (int) Math.floor(box.getMaxZ());
+
+        for (int x = minX; x <= maxX; x++) {
+            for (int y = minY; y <= maxY; y++) {
+                for (int z = minZ; z <= maxZ; z++) {
+                    Block block = world.getBlockAt(x, y, z);
+                    BoundingBox blockBox = block.getBoundingBox();
+
+                    // Check if the player's hitbox intersects this block's hitbox
+                    if (box.overlaps(blockBox)) {
+                        blocks.add(block);
+                    }
+                }
+            }
+        }
+        return blocks;
+    }
+
+}
